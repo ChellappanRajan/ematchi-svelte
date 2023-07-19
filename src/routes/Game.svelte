52eec9da9d67c86b208dbaa908b84d3ddd0fc200
Chellappan <vrchellappan@gmail.com>
2023-07-19 14:30:15 +0530

    passing data to game components

---
+++ src/routes/Game.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Grid from "./Grid.svelte";
+  import { LEVELS, type Level } from "./levels";
+
+ const level = LEVELS.at(0)!;
+ let size = level?.size;
+ let grid:string[] = create_grid(level);
+ let found:string[]= [];
+    console.log(grid);
+ function create_grid(level:Level){
+    const copy = structuredClone(level.emojis);
+    const pairs:string[] = [];
+    for(let i =0;i<level.size ** 2/2;i+=1){
+        const index = Math.floor(Math.random() * pairs.length);
+        const emoji = copy[index];
+        //Remove already selected emoji from copy
+        copy.splice(index,1);
+        pairs.push(emoji);
+    }
+    pairs.push(...pairs);
+    return pairs;
+ }
+
+</script>
+<div class="game">
+    <div class="info">
+
+    </div>
+    <div class="grid-container">
+        <Grid {grid}></Grid>
+    </div>
+    <div class="info">
+
+    </div>
+</div>
+
+<style>
+    .game{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        font-size: min(1vmin,0.5rem);
+    }
+    .info{
+        width: 80em;
+        height: 10em;
+        background: purple;
+    }
+    .grid-container{
+        width: 80em;
+        height: 80em;
+        background: teal    ;
+    }
+</style>
---
+++ src/routes/Square.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let emoji:string;
+</script>
+
+<div class="square">
+  <span>
+    {emoji}
+  </span>
+</div>
+
+<style>
+    .square{
+        display: flex;
+        align-items: center;
+        justify-self: center;
+    }
+    span{
+        font-size: 5em;
+    }
+</style>
